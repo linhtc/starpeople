22c7f9b276b86dac2afab759340234e85c4f379b
Leon <tplus.tcl@gmail.com>
2017-08-13 03:20:37 +0700

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ sys.path.insert(0, os.path.abspath('.'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 #extensions = ['sphinx.ext.autodoc']
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinxcontrib.httpdomain']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest']
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
