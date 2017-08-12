3e551f0dbd17748c4f51c07cfdb88e621ac26107
Leon <tplus.tcl@gmail.com>
2017-08-13 02:59:40 +0700

    test

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='sphinxcontrib-httpdomain',
+    name='httpdomain',
     version="1.5.0",
     description='Sphinx domain for HTTP APIs',
     author='Hong Minhee',
